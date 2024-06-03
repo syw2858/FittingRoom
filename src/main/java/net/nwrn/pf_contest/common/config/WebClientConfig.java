cd22d38716ee5494bfbfad745c9e48ed994ea51b
kwonryul <pkr08205@gmail.com>
2024-06-03 12:32:57 +0900

    WebClient Max MemorySize

--- src/main/java/net/nwrn/pf_contest/common/config/WebClientConfig.java
+++ src/main/java/net/nwrn/pf_contest/common/config/WebClientConfig.java
@@ -3,11 +3,17 @@ package net.nwrn.pf_contest.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {return WebClient.create();}
+    public WebClient webClient() {
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+            .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
+                .build();
+        return WebClient.builder().exchangeStrategies(exchangeStrategies).build();
+    }
 }
