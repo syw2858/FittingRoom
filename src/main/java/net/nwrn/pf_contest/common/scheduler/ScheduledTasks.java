d43062b127eb6a8c134802cba679cd917ea0e005
kwonryul <pkr08205@gmail.com>
2024-06-25 12:38:14 +0900

    스케줄러

--- src/main/java/net/nwrn/pf_contest/common/scheduler/ScheduledTasks.java
+++ src/main/java/net/nwrn/pf_contest/common/scheduler/ScheduledTasks.java
@@ -18,7 +18,7 @@ public class ScheduledTasks {
     private String topImageUrl = "https://d1hds1xxjs6al7.cloudfront.net/top/114/447";
     private String bottomImageUrl = "https://d1hds1xxjs6al7.cloudfront.net/bottom/72/450";
 
-    @Scheduled(cron = "0 0/5 0-5 * * ?")
+    @Scheduled(cron = "0 0/5 * * * ?")
     public void dummyRequest() throws URISyntaxException {
         URI uri = new URIBuilder().setScheme("http").setHost("localhost:4000").setPath("/fitting").build();
 
